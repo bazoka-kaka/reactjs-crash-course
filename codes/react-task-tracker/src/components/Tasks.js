b0877f6be9595b67a39a855e4601e38394ce864d
MOSSAD <tsukikagemaikaka1@gmail.com>
2022-02-23 14:22:24 +0700

    useEffect hook and fetch tasks from server

--- codes/react-task-tracker/src/components/Tasks.js
+++ codes/react-task-tracker/src/components/Tasks.js
@@ -3,13 +3,8 @@ import Task from "./Task";
 const Tasks = ({ onToggle, tasks, onDelete }) => {
   return (
     <>
-      {tasks.map((task) => (
-        <Task
-          onToggle={onToggle}
-          key={task.id}
-          onDelete={onDelete}
-          task={task}
-        />
+      {tasks.map((task, index) => (
+        <Task onToggle={onToggle} key={index} onDelete={onDelete} task={task} />
       ))}
     </>
   );
